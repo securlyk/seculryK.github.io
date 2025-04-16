9ee96999711d2f16d3253d01c3d9abf7a67b9046
【 𝔖𝔩𝔞𝔠𝔨𝔯𝔯ℑ𝔰𝔥 | ﾚズﾉ丂 🎲 】 <rh4p5od7@gmail.com>
2025-04-16 01:10:23 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,23 +131,3 @@ function delPassword() {
 	localStorage.removeItem("selenite.passwordAtt");
 	localStorage.removeItem("selenite.password");
 }
-
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
