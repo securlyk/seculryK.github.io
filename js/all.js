ac8a7c697921a1ab0205bebe7599a3558182f094
【 𝔖𝔩𝔞𝔠𝔨𝔯𝔯ℑ𝔰𝔥 | ﾚズﾉ丂 🎲 】 <rh4p5od7@gmail.com>
2025-04-16 00:20:34 -0500

    Update all.js

--- js/all.js
+++ js/all.js
@@ -71,7 +71,7 @@ document.addEventListener("keydown", (e) => {
 			alert("debugging off!");
 		} else {
 			document.cookie = "debugging=1";
-			alert("debugging on! access Test Server? copy this: https://slackerish.github.io./Debug/Test.html");
+			alert("debugging on!");
 		}
 		listofchars = "";
 	}
